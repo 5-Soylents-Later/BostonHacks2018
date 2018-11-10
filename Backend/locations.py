90f0838c29cee8ac9f3d6b11dcd6f25dd4fd9cb1
Ashley Deshields <adeshiel@bu.edu>
2018-11-10 14:50:43 -0500

    moved files

--- Backend/locations.py
+++ Backend/locations.py
@@ -1,11 +1,20 @@
 from Keys import GMaps
 import requests
-import pandas
+import pandas as pd
 
-def latlongAddress(lat, long):
+
+
+def latlongAddress(id):
+
+    bikes = pd.read_csv("Hubway_Stations_as_of_July_2017.csv")
+    lat = bikes.loc[id]['Latitude']
+    long = bikes.loc[id]['Longitude']
+    
     URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+ str(lat) + "," + str(long) + "&key=" + GMaps
     r = requests.get(url = URL)
     data = r.json()
     return data["results"][0]["formatted_address"]
+
+
 
 # print(latlongAddress(42.325333, -71.075354))
