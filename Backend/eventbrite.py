3474d30cec26bb9a378377f5fc53cc8360ae3656
KevinKByte <22605103+KevinKbyte@users.noreply.github.com>
2018-11-10 18:11:41 -0500

    cleaned up some of the json for eventbrite to only show the name, organizer, longitude, latitude, and localized_address_display
    Signed-off-by: KevinKByte <22605103+KevinKbyte@users.noreply.github.com>

--- Backend/eventbrite.py
+++ Backend/eventbrite.py
@@ -19,9 +19,21 @@ response = requests.get(
               "location.longitude": longitude, \
               #sort_by: “date”, “distance” and “best”. Prefix with a hyphen to reverse the order, e.g. “-date”
               "sort_by": sort_by, \
+              "expand": "organizer,venue"
               }
 )
 data = response.json()
 
-print(json.dumps(data, sort_keys=True, indent=4))
+new_data = []
 
+for event in data["events"]:
+    new_data += [ {\
+                   "name": event["name"]["text"], \
+                   "organizer": event["organizer"]["name"], \
+                   "description": event["description"]["html"], \
+                   "latitude": event["venue"]["latitude"], \
+                   "longitude": event["venue"]["longitude"], \
+                   "localized_address_display": event["venue"]["address"]["localized_address_display"], \
+                   } ]
+
+print(json.dumps(new_data, sort_keys=True, indent=4))
